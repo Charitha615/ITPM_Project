a18c6a67cb7b8f79c2492848d78d1cd2af614a6f
Charitha Wickramarathna <56013746+Charitha615@users.noreply.github.com>
2025-04-07 15:08:56 +0530

    updates

--- smart-tax/client/src/App.js
+++ smart-tax/client/src/App.js
@@ -1,20 +1,19 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/pages/Login';
+import Register from './components/pages/Register';
 
-import { AnimatePresence } from 'framer-motion';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import './App.css';
+
 
 function App() {
   return (
     <Router>
-      <AnimatePresence mode='wait'>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<Login />} />
-        </Routes>
-      </AnimatePresence>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+
+      </Routes>
     </Router>
   );
 }
